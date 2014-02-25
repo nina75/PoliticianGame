47fc34f15ee18b49cbd2a5f104ce6b4b31ef6c51
burnedd <penchev_p@live.com>
2014-02-25 15:02:55 +0200

    Update resource data

--- PoliticianCrusade/Game.cs
+++ PoliticianCrusade/Game.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
@@ -36,12 +37,12 @@ namespace PoliticianCrusade
             var walker = new Walker(64, 4);
 
             Console.SetCursorPosition(0, 33);
-            Console.WriteLine("Money: {0, 6} $  Use Arrows: ", baba.Money.Quantity);
-            Console.WriteLine("Cane:     {0, 3} %   UP", baba.Cane.RemainingPower);
-            Console.WriteLine("Bag:      {0, 3} %  DOWN  ", baba.Bag.RemainingPower);
-            Console.WriteLine("Umbrella: {0, 3} %  LEFT ", baba.Umbrella.RemainingPower);
-            Console.WriteLine("Gun:      {0, 3} %  RIGHT", baba.Gun.RemainingPower);
-            Console.WriteLine("Health:   {0, 3} %", baba.Health);
+            Console.WriteLine("Money:        $  Use Arrows: ");
+            Console.WriteLine("Cane:         %  UP");
+            Console.WriteLine("Bag:          %  DOWN  ");
+            Console.WriteLine("Umbrella:     %  LEFT ");
+            Console.WriteLine("Gun:          %  RIGHT");
+            Console.WriteLine("Health:       %");
             var objects =
                 new List<GameObject>()
                 {
@@ -67,16 +68,28 @@ namespace PoliticianCrusade
                 politician2.MoveBack(97, 15);
                 politician3.Move(0, 18);
 
+                UpdateResource(baba);
+
                 Thread.Sleep(100);
             }
         }
 
-        private static void UpdateResource(IResource resource)
+        private static void UpdateResource(GrandMom baba)
         {
-            Console.SetCursorPosition(resource.CoordXOnScreen, resource.CoordYOnScreen);
-            Console.Write("   ");
-            Console.SetCursorPosition(resource.CoordXOnScreen, resource.CoordYOnScreen);
-            Console.Write(resource.RemainingPower);
+            var allResources = baba.AllResources();
+
+            int newLiner = 0;
+
+            foreach (var resource in allResources)
+            {
+                Console.SetCursorPosition(10, 33 + newLiner++);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write(resource.RemainingPower);
+            }
+
+            Console.SetCursorPosition(10, 33 + newLiner);
+            Console.Write(baba.Health);
+            
         }
     }
 }
