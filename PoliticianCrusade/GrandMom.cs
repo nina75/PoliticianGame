39cfeafd4d648b0e167d5031551cf07f15b4afcf
AlexanderNikolov <investment.sis@gmail.com>
2014-02-25 17:41:37 +0200

    check

--- PoliticianCrusade/GrandMom.cs
+++ PoliticianCrusade/GrandMom.cs
@@ -27,7 +27,9 @@ namespace PoliticianCrusade
             this.Gun = new Gun();
         }
 
-        public GrandMom() { } //AN: празен конструктор? явно не е необходим щом работи без него :)
+        public GrandMom() 
+        {
+        } 
 
         public List<GameObject> EnemyList 
         {
@@ -198,7 +200,7 @@ namespace PoliticianCrusade
             return list;
         }
 
-        #region DrawImage
+        #region DrawObjectMethods
         public override void RenderImg()
         {
             Console.ForegroundColor = ConsoleColor.Red;
